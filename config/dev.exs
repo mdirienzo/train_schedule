9fd9fb6e876401dba7d0c8f572c0594eedf3b732
Michael DiRienzo <dirienzomichael@gmail.com>
2017-02-14 01:47:08 -0500

    all build tools configured

--- config/dev.exs
+++ config/dev.exs
@@ -11,8 +11,8 @@ config :train_schedule, TrainSchedule.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
-
+  watchers: [ ]
+# node: ["node_modules/webpack/bin/webpack.js", "--watch", "--colors", "--progress"]
 
 # Watch static and templates for browser reloading.
 config :train_schedule, TrainSchedule.Endpoint,
